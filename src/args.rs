cc282dbedbd4fe125d61c8ab90bfb42e584f006b
Sergey Bobretsov <s.bobretsov@gmail.com>
2022-11-01 15:56:41 +0300

    add json response

--- src/args.rs
+++ src/args.rs
@@ -1,11 +1,11 @@
 use clap::Parser;
 
-#[derive(Parser, Debug,Clone)]
+#[derive(Parser, Debug, Clone)]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
     /// Address:port to listen
-    #[arg(short, long, default_value = "0.0.0.0:8080")]
-    pub listen: String,
+    #[arg(short, long, default_value_t = 8080)]
+    pub port: u16,
     /// Root path to save blobs
     #[arg(short, long)]
     pub root: String,
--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,14 @@ extern crate core;
 use crate::args::Args;
 use crate::blob::storage::Container;
 use clap::Parser;
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
 use std::fs::File;
 use std::path::Path;
 use std::sync::{Arc, RwLock};
 use std::time::{SystemTime, UNIX_EPOCH};
-use hyper::{Body, Method, Request, Response, Server, StatusCode};
-use hyper::service::{make_service_fn, service_fn};
 use tokio::sync::mpsc::{unbounded_channel, UnboundedSender};
 use tokio::task;
-
 
 mod args;
 mod blob;
@@ -21,17 +20,14 @@ const WRITER_COUNT: u32 = 10;
 const OBJECTS_IN_CONTAINER: u32 = 1_000;
 
 #[derive(Debug)]
-struct PostData{
+struct PostData {
     data: Vec<u8>,
     writer_id: u32,
 }
 
 impl PostData {
-    fn new(writer_id:u32, data:Vec<u8>)-> Self{
-        Self{
-            data,
-            writer_id
-        }
+    fn new(writer_id: u32, data: Vec<u8>) -> Self {
+        Self { data, writer_id }
     }
 }
 
@@ -40,9 +36,9 @@ async fn main() -> std::io::Result<()> {
     let args: Args = Args::parse();
     let mut senders = Vec::new();
 
-    println!("root = {}",args.root);
+    println!("root = {}", args.root);
 
-    for type_id in 0..TYPE_COUNT-1 {
+    for type_id in 0..TYPE_COUNT - 1 {
         let (sender, mut receiver) = unbounded_channel();
         senders.push(sender);
         let root = args.root.clone();
@@ -55,30 +51,31 @@ async fn main() -> std::io::Result<()> {
                 let mut container = Container::new(type_id);
                 for _ in 0..OBJECTS_IN_CONTAINER {
                     let obj: PostData = receiver.recv().await.unwrap();
-                    container.push(obj.writer_id,obj.data.as_slice());
+                    container.push(obj.writer_id, obj.data.as_slice());
                 }
-                let path = Path::new(root.as_str()).join(format!("type{}_{}.blob", type_id, creation_time));
-                println!("{}",path.to_str().unwrap());
+                let path = Path::new(root.as_str())
+                    .join(format!("type{}_{}.blob", type_id, creation_time));
+                println!("{}", path.to_str().unwrap());
                 let file = File::create(path).unwrap();
                 container.save_to_file(file).unwrap();
             }
         });
     }
     let senders = Arc::new(RwLock::new(senders));
-    let addr = ([0, 0, 0, 0], 8080).into();
+    let addr = ([0, 0, 0, 0], args.port).into();
     let service = make_service_fn(move |_| {
         let senders = senders.clone();
         async move {
-            Ok::<_, hyper::Error>(service_fn(move |_req|{
+            Ok::<_, hyper::Error>(service_fn(move |_req| {
                 let senders = senders.clone();
-                handler(_req,senders)
+                handler(_req, senders)
             }))
         }
     });
 
     let server = Server::bind(&addr).serve(service);
 
-    println!("Listening on http://{}", addr);
+    println!("Listening {}", addr);
 
     if let Err(e) = server.await {
         eprintln!("server error: {}", e);
@@ -87,48 +84,55 @@ async fn main() -> std::io::Result<()> {
     Ok(())
 }
 
-async fn handler(req: Request<Body>,senders:Arc<RwLock<Vec<UnboundedSender<PostData>>>>) -> Result<Response<Body>, hyper::Error> {
-    let (type_id,writer_id) = match  parse_path(req.uri().path()) {
+async fn handler(
+    req: Request<Body>,
+    senders: Arc<RwLock<Vec<UnboundedSender<PostData>>>>,
+) -> Result<Response<Body>, hyper::Error> {
+    let (type_id, writer_id) = match parse_path(req.uri().path()) {
         None => {
-            let mut bad_request = Response::default();
-            *bad_request.status_mut() = StatusCode::BAD_REQUEST;
-            return Ok(bad_request)
+            return Ok(Response::new(Body::from(
+                r#"{ "state": -1,"reason"=41,desc="invalid path, need /type_id/N/writer_id/K" }"#
+                    .to_string(),
+            )))
         }
-        Some(type_id ) => { type_id}
+        Some(type_id) => type_id,
     };
-    if type_id >= TYPE_COUNT || writer_id >= WRITER_COUNT{
-        let mut bad_request = Response::default();
-        *bad_request.status_mut() = StatusCode::BAD_REQUEST;
-        return Ok(bad_request)
+    if type_id >= TYPE_COUNT || writer_id >= WRITER_COUNT {
+        return Ok(Response::new(Body::from(
+            r#"{ "state": -1,"reason"=42,desc="invalid type_id or writer_id value" }"#.to_string(),
+        )));
     }
     match req.method() {
         &Method::POST => {
             let whole_body = hyper::body::to_bytes(req.into_body()).await?.to_vec();
             let senders = senders.read().unwrap();
-            let sender = senders.get(type_id as usize ).unwrap();
-            sender.send(PostData::new(writer_id,whole_body)).unwrap();
-            Ok(Response::new(Body::from(format!("OK"))))
+            let sender = senders.get(type_id as usize).unwrap();
+            sender.send(PostData::new(writer_id, whole_body)).unwrap();
+            Ok(Response::new(Body::from(r#"{ "state": 0 }"#.to_string())))
         }
         _ => {
             let mut not_found = Response::default();
-            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            *not_found.status_mut() = StatusCode::METHOD_NOT_ALLOWED;
             Ok(not_found)
         }
     }
 }
 
-fn parse_path(path: &str) -> Option<(u32,u32)>{
+fn parse_path(path: &str) -> Option<(u32, u32)> {
     let parts: Vec<&str> = path.split('/').collect();
-    if parts.len()<5 || parts[1].to_lowercase() != "type_id" || parts[3].to_lowercase() != "writer_id" {
+    if parts.len() < 5
+        || parts[1].to_lowercase() != "type_id"
+        || parts[3].to_lowercase() != "writer_id"
+    {
         return None;
     }
-    let type_id = match parts[2].parse::<u32>(){
-        Ok(type_id) => { type_id}
-        Err(_) => { return None }
+    let type_id = match parts[2].parse::<u32>() {
+        Ok(type_id) => type_id,
+        Err(_) => return None,
     };
     let writer_id = match parts[4].parse::<u32>() {
-        Ok(writer_id) => { writer_id}
-        Err(_) => { return None }
+        Ok(writer_id) => writer_id,
+        Err(_) => return None,
     };
-    Some((type_id,writer_id))
+    Some((type_id, writer_id))
 }
